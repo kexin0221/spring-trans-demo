75d5e02fb3a26e13455b151518de419441a520b7
mou-kexin <2584857586@qq.com>
2025-11-24 20:36:40 +0800

    Initial commit

---
+++ src/test/java/com/bite/springtransdemo/SpringTransDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bite.springtransdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTransDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
